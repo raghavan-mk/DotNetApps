9a6b2d36cc1f1936a0fa3d801e41a0c476669e9b
raghavan-mk <raghavan.mk@gmail.com>
2022-01-15 12:46:43 +0530

    Minor refactorings

--- Wordle/Program.cs
+++ Wordle/Program.cs
@@ -1,4 +1,6 @@
-﻿using static System.Console;
+﻿using System.Globalization;
+using System.Net.Mime;
+using static System.Console;
 
 var input = "stare";
 
@@ -8,37 +10,27 @@ absent.Add('t');
 absent.Add('r');
 absent.Add('e');
 absent.Add('m');
-absent.Add('g');
-absent.Add('q');
-absent.Add('j');
-absent.Add('k');
-absent.Add('v');
-absent.Add('h');
-absent.Add('u');
-absent.Add('o');
-
+absent.Add('l');
 var present = new Dictionary<char, List<int>>();
-present.Add('a', new List<int> {2,3,4});
-present.Add('c', new List<int> {0,3});
-present.Add('p', new List<int> {2});
-present.Add('n', new List<int> {4});
+present.Add('a', new List<int> {0, 2});
 
 var correct = new char[5];
 correct[1] = 'a';
+correct[3] = 'i';
+correct[4] = 'c';
+Play();
 
-var nextWordIsGuessed = false;
-var count = 0;
-var wordle = new DotNETApps.Wordle(absent, present, correct);
-
-wordle.SetGuessedWords("stare");
-wordle.SetGuessedWords("magma");
-wordle.SetGuessedWords("qajaq");
-wordle.SetGuessedWords("vauch");
-wordle.SetGuessedWords("capon");
-
-while (!nextWordIsGuessed && count < 12972)
+void Play()
 {
-    nextWordIsGuessed = wordle.TryGetNextWord(out var nextWord);
-    wordle.SetGuessedWords(nextWord);
-    WriteLine($"{count++}:{nextWord}");
+    var nextWordIsGuessed = false;
+    var count = 0;
+    var wordle = new DotNETApps.Wordle(absent!, present!, correct!);
+    wordle.SetGuessedWords("stare");
+    wordle.SetGuessedWords("aulic");
+    wordle.SetGuessedWords("malic");
+    while (!nextWordIsGuessed && count < 12972)
+    {
+        nextWordIsGuessed = wordle.TryGetNextWord(out var nextWord);
+        WriteLine($"{count++}:{nextWord}");
+    }
 }
--- Wordle/Wordle.cs
+++ Wordle/Wordle.cs
@@ -5,8 +5,13 @@ internal class Wordle
     private readonly string[] _corpus;
     private readonly HashSet<string> _guessedWords = new();
     private readonly HashSet<char> _absent;
+    
+    //letters present but not in right position
     private readonly Dictionary<char, List<int>> _present;
+    
+    //letters present and in right position
     private readonly char[] _correct;
+    
     private readonly HashSet<int> _guessedNumbers = new();
 
     public Wordle(HashSet<char> absent, Dictionary<char, List<int>> present, char[] correct)
@@ -16,6 +21,7 @@ internal class Wordle
         _present = present;
         _correct = correct;
     }
+
     public bool TryGetNextWord(out string nextWord)
     {
         var random = GetNextRandomNumber();
@@ -25,39 +31,35 @@ internal class Wordle
         if (_guessedWords.Contains(nextGuess))
             return false;
 
-        for (var i = 0; i < nextGuess.Length; i++)
-        {
-            if (!KeepTheLetter(nextGuess[i], i))
-                return false;
-        }
+        if (nextGuess.Where((t, i) => !KeepTheLetter(t, i)).Any())
+            return false;
 
+        _guessedWords.Add(nextGuess);
         return KeepTheWord(nextGuess);
     }
-    
-    public void SetGuessedWords(string guessedWord) => 
-        _guessedWords.Add(guessedWord);
 
     bool KeepTheWord(string word)
     {
-        for (var i = 0; i < _correct.Length; i++)
-        {
-            var c = _correct[i];
-            if (c == '\0') continue;
-            if (word.IndexOf(c) != i)
-                return false;
-        }
+        //ensure all correct letters are present
+        if (_correct.Where((c, i) => c != '\0' && word.IndexOf(c) != i).Any())
+            return false;
 
+        //ensure all present letters are present
         return _present.Keys.All(word.Contains);
     }
 
     bool KeepTheLetter(char letter, int index)
     {
+        //if a letter is not present in both present as well as absent then it is kept
         if (!_present!.ContainsKey(letter))
             return !_absent!.Contains(letter);
 
+        //if present ensure, it is present in a different position/index
         return !_present[letter].Contains(index);
     }
 
+    //returns a random number between 0 and corpus.length 
+    //which is not in the guessedNumbers
     int GetNextRandomNumber()
     {
         var nxtNumber = new Random().Next(0, 12972);
@@ -66,16 +68,19 @@ internal class Wordle
         _guessedNumbers.Add(nxtNumber);
         return nxtNumber;
     }
+    
+    public void SetGuessedWords(string word) => _guessedWords.Add(word);
 }
 
 internal static class Corpus
 {
     private static readonly string[] Words;
+
     static Corpus()
     {
         Words = File.ReadAllText("corpus.txt").Split(',')
             .Select(w => w.Replace("\"", "").Trim()).ToArray();
     }
-    
+
     public static string[] GetCorpus() => Words;
 }
