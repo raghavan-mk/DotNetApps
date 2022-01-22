cde5cdf5ba5b66d539ca46801fcbd6f72c02a0d4
raghavan-mk <raghavan.mk@gmail.com>
2022-01-22 13:35:12 +0530

    Removed try/catch

--- WordleLib/Helper.cs
+++ WordleLib/Helper.cs
@@ -6,31 +6,26 @@ namespace WordleLib;
 
 public static class Helper
 {
-    private static readonly string[] Appreciations = {"Genius","Magnificent","Impressive","Splendid","Great", "Phew" };
+    private static readonly string[] Appreciations =
+        {"Genius", "Magnificent", "Impressive", "Splendid", "Great", "Phew"};
+
     public static Task<string> GetColor(Page page, int row, int cell) =>
         page.EvaluateExpressionAsync<string>($"window.getComputedStyle(" +
-                                                   "document.querySelector(\"body > game-app\")." +
-                                                   $"shadowRoot.querySelector(\"#board > game-row:nth-child({row})\")." +
-                                                   $"shadowRoot.querySelector(\"div > game-tile:nth-child({cell})\")." +
-                                                   "shadowRoot.querySelector(\"div\")).getPropertyValue(\"background-color\")");
+                                             "document.querySelector(\"body > game-app\")." +
+                                             $"shadowRoot.querySelector(\"#board > game-row:nth-child({row})\")." +
+                                             $"shadowRoot.querySelector(\"div > game-tile:nth-child({cell})\")." +
+                                             "shadowRoot.querySelector(\"div\")).getPropertyValue(\"background-color\")");
+
     public static async Task<string> GetWordleSln(Page page) =>
         await page.EvaluateExpressionAsync<string>($"JSON.parse(localStorage[\"gameState\"])[\"solution\"]");
 
     public static string ParseRgb(string color)
     {
-        try
-        {
-            var rgb = Regex.Split(color, @"rgb\((\d{1,3}),\s*(\d{1,3}),\s*(\d{1,3})\)")
-                .Where(c => c != "") // for some reason spaces were added too so we need to remove them
-                .Select(int.Parse)
-                .ToArray();
-            return GetColorCode(rgb);
-        }
-        catch(Exception e)
-        {
-            Console.WriteLine(color);
-            throw;
-        }
+        var rgb = Regex.Split(color, @"rgb\((\d{1,3}),\s*(\d{1,3}),\s*(\d{1,3})\)")
+            .Where(c => c != "") // for some reason spaces were added too so we need to remove them
+            .Select(int.Parse)
+            .ToArray();
+        return GetColorCode(rgb);
     }
 
     public static string GetColorCode(IReadOnlyList<int> rgb) => Color.FromArgb(rgb[0], rgb[1], rgb[2]).Name;
@@ -44,6 +39,6 @@ public static class Helper
         "ffb59f3b" or "ffc9b458" => 'p',
         _ => throw new ArgumentOutOfRangeException(nameof(color), color, null)
     };
-    
+
     public static string GetAppreciation(int attempt) => Appreciations[attempt];
 }
