95c60c82e86bb5a5af3f3a707ec4d0c4afea1c2e
raghavan-mk <raghavan.mk@gmail.com>
2022-01-23 17:35:55 +0530

    Modified random number generator

--- WordleWeb/Program.cs
+++ WordleWeb/Program.cs
@@ -4,139 +4,159 @@ using Spectre.Console;
 using WordleWeb;
 using static System.Console;
 
-const string wordleUrl = "https://www.powerlanguage.co.uk/wordle/";
-var headless = true;
-var currentWord = "stare";
+BrowserFetcher? browserFetcher = null;
+Browser? browser = null;
+Page? page = null;
 
-var cmdLineArgs = Environment.GetCommandLineArgs();
-if (cmdLineArgs.Length > 1)
+try
 {
-    cmdLineArgs.Skip(1).ToList().ForEach(c =>
+    const string wordleUrl = "https://www.powerlanguage.co.uk/wordle/";
+    var headless = true;
+    var currentWord = "stare";
+
+    var cmdLineArgs = Environment.GetCommandLineArgs();
+    if (cmdLineArgs.Length > 1)
     {
-        if (c.StartsWith("-h="))
+        cmdLineArgs.Skip(1).ToList().ForEach(c =>
         {
-            var t = bool.TryParse(c.Split('=')[1], out headless);
-        }
-        if(c.StartsWith("-w="))
-        {
-            if(c.Split('=')[1].Length == 5)
+            if (c.StartsWith("-h="))
             {
-                currentWord = c.Split('=')[1];
+                var t = bool.TryParse(c.Split('=')[1], out headless);
             }
-        }
+
+            if (c.StartsWith("-w="))
+            {
+                if (c.Split('=')[1].Length == 5)
+                {
+                    currentWord = c.Split('=')[1];
+                }
+            }
+        });
+    }
+
+    browserFetcher = new BrowserFetcher();
+    browserFetcher.DownloadProgressChanged += (sender, args) =>
+    {
+        AnsiConsole.Markup(
+            $"\r [bold green] Downloading Chromium Driver[/] [bold red]{args.ProgressPercentage}%[/]");
+    };
+
+    await browserFetcher.DownloadAsync();
+    browser = await Puppeteer.LaunchAsync(new LaunchOptions
+    {
+        Headless = headless
     });
-}
 
-var browserFetcher = new BrowserFetcher();
-browserFetcher.DownloadProgressChanged += (sender, args) =>
-{
-    AnsiConsole.Markup($"\r [bold green] Downloading Chromium Driver[/] [bold red]{args.ProgressPercentage}%[/]");
-};
+    Clear();
+    var output = new Output();
+    output.OutputInAscii("Wordle");
 
-await browserFetcher.DownloadAsync();
-await using var browser = await Puppeteer.LaunchAsync(new LaunchOptions
-{
-    Headless = headless
-});
+    page = await browser.NewPageAsync();
+    await page.GoToAsync(wordleUrl);
 
-Clear();
-var output = new Output();
-output.OutputInAscii("Wordle");
+    var element = await
+        page.EvaluateExpressionHandleAsync(
+            "document.querySelector(\"body > game-app\").shadowRoot.querySelector(\"#game > game-modal\")" +
+            ".shadowRoot.querySelector(\"div > div > div > game-icon\")" +
+            ".shadowRoot.querySelector(\"svg\")") as ElementHandle;
 
-await using var page = await browser.NewPageAsync();
-await page.GoToAsync(wordleUrl);
+    await Task.Delay(100);
+    await element?.ClickAsync()!;
 
-var element = await
-    page.EvaluateExpressionHandleAsync(
-        "document.querySelector(\"body > game-app\").shadowRoot.querySelector(\"#game > game-modal\")" +
-        ".shadowRoot.querySelector(\"div > div > div > game-icon\")" +
-        ".shadowRoot.querySelector(\"svg\")") as ElementHandle;
-
-await Task.Delay(100);
-await element?.ClickAsync()!;
-
-List<string> colors = new(5);
-var wordle = new Wordle(currentWord);
-var completed = false;
-var attempt = 0;
+    List<string> colors = new(5);
+    var wordle = new Wordle(currentWord);
+    var completed = false;
+    var attempt = 0;
 
 
 // number of attempts to find a word - 6 is the max
 // also means to increment the row count by 1 to read next word
-for (var i = 1; i < 7; i++) 
-{
-    await page.TypeAsync("body > game-app", currentWord);
-    await page.Keyboard.PressAsync(Key.Enter);
-    await Task.Delay(1500); // delay to allow the word to be typed and get the colors of the word
-
-    for (var j = 1; j < 6; j++)
+    for (var i = 1; i < 7; i++)
     {
-        var rgb = await Helper.GetColor(page, i, j);
-        await Task.Delay(100);
-        colors.Add(Helper.ParseRgb(rgb));
-    }
-    output.Addrow(currentWord,colors);
-    // dark and non-dark mode   
-    if (!colors.All(c => c is "ff538d4e" or "ff6aaa64")) 
-        GetNextWord();
-    else
-    {
-        completed = true;
-        attempt = i;
-        break;
-    }
+        await page.TypeAsync("body > game-app", currentWord);
+        await page.Keyboard.PressAsync(Key.Enter);
+        await Task.Delay(1500); // delay to allow the word to be typed and get the colors of the word
 
-    colors.Clear();
-}
+        for (var j = 1; j < 6; j++)
+        {
+            var rgb = await Helper.GetColor(page, i, j);
+            await Task.Delay(100);
+            colors.Add(Helper.ParseRgb(rgb));
+        }
+
+        output.Addrow(currentWord, colors);
+        // dark and non-dark mode   
+        if (!colors.All(c => c is "ff538d4e" or "ff6aaa64"))
+            GetNextWord();
+        else
+        {
+            completed = true;
+            attempt = i;
+            break;
+        }
+
+        colors.Clear();
+    }
 //await Task.Delay(1000);
 
-if (completed)
-{
-    output.OutputInAscii($"{Helper.GetAppreciation(attempt - 1)}!");
-}
-else
-{
-    AnsiConsole.Write(new Markup($"[bold red]Could not find the word[/]"));
-    WriteLine();
-    var wordleOfTheDay = await Helper.GetWordleSln(page);
-    AnsiConsole.Write(new Markup($"[bold yellow]Solution[/] [bold green]{wordleOfTheDay}[/]"));
-}
-
-await page.CloseAsync();
-
-void GetNextWord()
-{
-    for (var i = 0; i < colors.Count; i++)
+    if (completed)
     {
-        var c = Helper.EvalColorCode(colors[i]);
-        var cl = currentWord[i];
-        switch (c)
+        output.OutputInAscii($"{Helper.GetAppreciation(attempt - 1)}!");
+    }
+    else
+    {
+        AnsiConsole.Write(new Markup($"[bold red]Could not find the word[/]"));
+        WriteLine();
+        var wordleOfTheDay = await Helper.GetWordleSln(page);
+        AnsiConsole.Write(new Markup($"[bold yellow]Solution[/] [bold green]{wordleOfTheDay}[/]"));
+    }
+
+    await page.CloseAsync();
+
+    void GetNextWord()
+    {
+        for (var i = 0; i < colors.Count; i++)
         {
-            case 'c':
-                wordle.Correct[i] = cl;
-                break;
-            case 'a' when !wordle.Correct.Contains(cl)
-                          && !wordle.Absent.Contains(cl):
-                wordle.Absent.Add(cl);
-                break;
-            case 'p':
+            var c = Helper.EvalColorCode(colors[i]);
+            var cl = currentWord[i];
+            switch (c)
             {
-                if (!wordle.Present.ContainsKey(cl))
-                    wordle.Present.Add(cl, new List<int> {i});
-                else
-                    wordle.Present[cl].Add(i);
-                break;
+                case 'c':
+                    wordle.Correct[i] = cl;
+                    break;
+                case 'a' when !wordle.Correct.Contains(cl)
+                              && !wordle.Absent.Contains(cl):
+                    wordle.Absent.Add(cl);
+                    break;
+                case 'p':
+                {
+                    if (!wordle.Present.ContainsKey(cl))
+                        wordle.Present.Add(cl, new List<int> {i});
+                    else
+                        wordle.Present[cl].Add(i);
+                    break;
+                }
             }
         }
+
+        var wordIsGuessed = false;
+        var count = 0;
+        while (!wordIsGuessed && count++ < 12792)
+        {
+            wordIsGuessed = wordle.TryGetNextWord(out currentWord);
+            // WriteLine($"{count++}:{currentWord}");
+        }
     }
-    
-    var wordIsGuessed = false;
-    var count = 0;
-    while (!wordIsGuessed && count++ < 12792)
-    {
-        wordIsGuessed = wordle.TryGetNextWord(out currentWord);
-        // WriteLine($"{count++}:{currentWord}");
-    }
+}
+catch (Exception e)
+{
+    AnsiConsole.WriteException(e);
+}
+finally
+{
+    page?.Dispose();
+    browser?.Dispose();
+    browserFetcher?.Dispose();
 }
 
 // dark mode colors
